6567ffc56564c6343fd890a192381e77ff2e7593
jatin <sandilya.jatin@gmail.com>
2024-05-23 13:43:19 +0530

    fix: firebase app being init multiple times

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -13,11 +13,23 @@ console.log('serviceAccount', serviceAccount);
 
 @Injectable()
 export class UsersService {
+  private readonly firebaseApp: any;
   constructor(
     private prisma: PrismaService,
     private passwordService: PasswordService,
     private readonly configService: ConfigService,
-  ) {}
+  ) {
+    if (!admin.apps.length) {
+      this.firebaseApp = admin.initializeApp(
+        {
+          credential: admin.credential.cert(
+            serviceAccount as admin.ServiceAccount,
+          ),
+        },
+        'unswipe',
+      );
+    }
+  }
 
   async updateUser(userId: string, newUserData: SignupInput) {
     return this.prisma.user.upsert({
@@ -36,15 +48,9 @@ export class UsersService {
   async createUserIfDoesntExist(id: string, userInput?: Partial<SignupInput>) {
     let user = await this.prisma.user.findFirst({ where: { id: id } });
     if (!user) {
-      const app = admin.initializeApp(
-        {
-          credential: admin.credential.cert(
-            serviceAccount as admin.ServiceAccount,
-          ),
-        },
-        'unswipe',
-      );
-      const firebaseCustomToken = await app.auth().createCustomToken(id);
+      const firebaseCustomToken = await this.firebaseApp
+        .auth()
+        .createCustomToken(id);
       user = await this.updateUser(id, {
         id: id,
         phone: id,
